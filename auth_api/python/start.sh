5f84c6a3e04418519124feb3cc80c58ec4aade2e
Lorenzo Alfonso Ramirez Zarate <alfonso09.lr@gmail.com>
2023-02-04 17:01:46 -0600

    Removing sensitive data from start bash script

--- auth_api/python/start.sh
+++ auth_api/python/start.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-app_name="wize-alfonso-ramirez:latest"
+app_name='wize-alfonso-ramirez:latest'
+database_user=''
+database_password=''
+database_url=''
+database_port='3306'
+database_name=''
+jwt_secret_key=''
+
+database_uri="mysql+pymysql://${database_user}:${database_password}@${database_url}/${database_name}"
+
 
 sudo docker build -t ${app_name} .
 
-sudo docker run -d -p 8000:5000 ${app_name}
+sudo docker run -d \
+	-e DATABASE_URI=${database_uri} \
+	-e JWT_SECRET_KEY=${jwt_secret_key} \
+	-p 8000:5000 \
+	${app_name}
